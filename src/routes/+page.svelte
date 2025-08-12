9d5855a5df3cb6c4a03f1b2afd53a812738ac818
KaileyLiou <kaileyliou09@gmail.com>
2025-08-12 00:05:53 -0400

    added script

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,35 @@
 <script>
+    @prerender
+    import { base } from "$app/paths";
+    import { onMount } from "svelte";
+    let loaded = $state(false);
+
+    let facts = $state([
+        {
+            "title": "Loading...",
+            "description": "",
+            "icon": "",
+            "id": 0
+        }
+    ]);
+
+    let currentFact = $state(1);
+    let totalFacts = $state(0);
+
+    let fact_description = $state("");
+    let fact_title = $state("Loading...");
+    let fact_icon = $state("");
     
+    onMount(function() {
+        fetch(`${base}/facts.json`)
+            .then(response => response.json())
+            .then(data => {
+                facts = data;
+                totalFacts = facts.length;
+                loaded = true;
+            });
+    })
+
 </script>
 
 <style>
@@ -10,5 +40,5 @@
     }
 </style>
 
-<h1>Save the Sea Turtles 🐢</h1>
+<h1>Save the Sea Turtles</h1>
 <p>All six sea turtle species are threatened or endangered</p>
