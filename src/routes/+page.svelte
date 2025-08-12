920baf217a7b2a8e90396ffb4eb06c6a9df563ba
KaileyLiou <kaileyliou09@gmail.com>
2025-08-11 23:46:16 -0400

    added style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<style>
+    h1 {
+        color: #b3e5fc;
+        font-family: 'Arial', sans-serif;
+        text-align: center;
+    }
+</style>
+
 <h1>Save the Sea Turtles 🐢</h1>
 <p>All six sea turtle species are threatened or endangered</p>
